b920d575da0a8e763c1fd373803164256bb36a11
Retajalotaibi <63907341+Retajalotaibi@users.noreply.github.com>
2022-03-04 17:20:23 +0300

    added a project

---
+++ ios-cw-3/ios-cw-3/ios_cw_3App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cw_3App.swift
+//  ios-cw-3
+//
+//  Created by Retaj Al-Otaibi on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_cw_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
